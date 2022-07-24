96b888252ae44681907b6260b5c84f2b994239c1
georgediepiriye <georgediepiriye4u@gmail.com>
2022-07-24 22:45:05 +0100

    added deployement script and hardhat config

---
+++ hardhat-tutorial/constants/index.js
@@ -0,0 +1,5 @@
+// Replace the value with your NFT contract address
+const FIGHTPUNKS_NFT_CONTRACT_ADDRESS =
+  "0x808bd9a9453B96499Cc11Bee5bE4aCb44B21D908";
+
+module.exports = { FIGHTPUNKS_NFT_CONTRACT_ADDRESS };
